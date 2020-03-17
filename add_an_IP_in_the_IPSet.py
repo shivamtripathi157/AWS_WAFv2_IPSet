abfea673f55a98b2920a0f9e2a0f92ece89284e4
SHIVAM TRIPATHI <shivamtripathi157@gmail.com>
2020-03-17 20:59:33 +0530

    Add files via upload

---
+++ add_an_IP_in_the_IPSet.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+
+client = boto3.client('wafv2')
+
+def lambda_handler(event, context):
+    getMyIPSet = client.get_ip_set(
+    Name='blocklist',
+    Scope='REGIONAL',
+    Id='47a052b7-c918-4440-a341-911a17dfc9e7'
+    )
+
+    lockToken=getMyIPSet["LockToken"]
+    myAddresses= getMyIPSet["IPSet"]['Addresses']
+    myAddresses.append('3.9.9.4/32')
+
+    updateMyIpset = client.update_ip_set(
+    Name='blocklist',
+    Scope='REGIONAL',
+    Id='47a052b7-c918-4440-a341-911a17dfc9e7',
+    Addresses=myAddresses,
+    LockToken=lockToken
+   )
+
+    return {
+        "newAddresses":myAddresses
+    }
